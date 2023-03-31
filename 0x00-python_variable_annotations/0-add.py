162be819068dfb85099b1cd18001b74596d7c0ec
robertokoba7 <okobarobert7@gmail.com>
2023-03-31 11:54:57 +0300

    [add]: Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
-
 """
 function add takes a float a and a float b as arguments and returns their sum as a float
 """
+
+
 def add(a: float, b: float) -> float:
     """return sum of float number"""
     return (a + b)
