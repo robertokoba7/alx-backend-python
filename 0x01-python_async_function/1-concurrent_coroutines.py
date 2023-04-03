bd1dee0816d909e7ac2420d50b93d956e301c9ba
robertokoba7 <okobarobert7@gmail.com>
2023-04-03 17:15:15 +0300

    [add] multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
      return list of all delays which is a float values
