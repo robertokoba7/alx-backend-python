66cd8afdc4f6bb2e85f6ff18b6204ac3eddd0364
robertokoba7 <okobarobert7@gmail.com>
2023-04-03 17:20:49 +0300

    [add] multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,16 +14,17 @@ async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
      return list of all delays which is a float values
     """
-    delays: List[float] = []
-    tasks: List = []
+    delays: List[float] = [] #  Define an empty list called delays to store the float values
+    tasks: List = [] # Define an empty list called tasks to store the tasks
 
     for _ in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
+        task = asyncio.create_task(wait_random(max_delay)) # Create a new task using wait_random() and add it to tasks
+        tasks.append(task)
         tasks.append(task)
 
-    for task in asyncio.as_completed(tasks):
+    for task in asyncio.as_completed(tasks): # Loop over the tasks as they are completed
 
-        delay = await task
-        delays.append(delay)
+        delay = await task # Wait for the task to complete and store the result in delay
+        delays.append(delay) # Add the delay value to the delays list
 
-    return sorted(delays)
+    return sorted(delays) # Sorted the delays in ascending order
