425121091a9fd25ad2adc3bdb067a70ac7fba491
robertokoba7 <okobarobert7@gmail.com>
2023-04-04 12:36:22 +0300

    [add] Creating async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Creating Async Generator using random module
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Corotine that takes no args
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
