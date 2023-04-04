4d8df72c40daf936cdb2a42cfdc4f5cbb97903e0
robertokoba7 <okobarobert7@gmail.com>
2023-04-05 00:07:50 +0300

    [add] Coroutine execution in parallel

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,16 +5,16 @@ Async Comprehension and Parallel execution using asyncio.gather
 import asyncio
 import time
 from typing import List
-
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """
     measure the total runtime and return it.
     """
     start_time = time.perf_counter()
     my_list = [asyncio.create_task(async_comprehension()) for i in range(4)]
     result = await asyncio.gather(*my_list)
+    elapse = time.perf_counter()
     total_time = time.perf_counter() - start_time
     return total_time
