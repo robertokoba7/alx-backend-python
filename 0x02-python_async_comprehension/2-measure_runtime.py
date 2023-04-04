de8a71053f5be6c79e2adeedcea8071dbc26073a
robertokoba7 <okobarobert7@gmail.com>
2023-04-04 17:36:59 +0300

    [add] Async Comprehension and Parallel Execution

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 """
-Async Comprehension and Parallel execution to asyncio.gather.
+Async Comprehension and Parallel execution using asyncio.gather
 """
-from async_generator import async_generator, async_comprehension
-from random import uniform
+
 from typing import List
 from time import perf_counter
 
 
+async_generator = __import__('0-async_generator').async_generator
+async_comprehension = __import__(1-async_comprehension).async_comprehension
+
+
 async def measure_runtime() -> float:
     """
-    `measure_runtime` coroutine uses `asyncio.gather` to run four instances
+    measure the total runtime and return it.
     """
-    start_time = perf_counter()
+    start_time = asyncio.get_running_loop().time()
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
-    elapse = perf_counter()
+    elapse = asyncio.get_running_loop().time()
     return elapse - start_time
